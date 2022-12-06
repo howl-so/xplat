d4816e58a22c1053a230a55dddc498ebe0294816
runner <runner@Mac-1670369794564.local>
2022-12-06 23:44:41 +0000

    KMM SPM package release for 0.0.7

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let remoteKotlinUrl = "https://api.github.com/repos/matt-ramotar/Howl/releases/assets/87262604.zip"
+let remoteKotlinChecksum = "847a6c5bb10f27487ea7c4bba673092c2304af3a99e74a70981f427591d1f9e8"
+let packageName = "StoreKit"
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
